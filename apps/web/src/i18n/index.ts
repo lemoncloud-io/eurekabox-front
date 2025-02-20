f96b386c3ec4aa3dac9d954f4da571c938604c85
Louis <46361622+louis-lemon@users.noreply.github.com>
2025-02-20 16:31:31 +0900

    헤더에 x-lemon-language 사용 (#43)
    * chore: update lemon-web-core dependency
    
    * feat: persist language preference in local storage
    
    * feat: add query retry to improve resilience

--- apps/web/src/i18n/index.ts
+++ apps/web/src/i18n/index.ts
@@ -2,10 +2,14 @@ import { initReactI18next } from 'react-i18next';
 
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import ChainedBackend from 'i18next-chained-backend';
+import LocalStorageBackend from 'i18next-localstorage-backend';
 import Backend from 'i18next-xhr-backend';
 
-i18n.use(Backend)
-    .use(LanguageDetector)
+import { ENV, LANGUAGE_KEY, PROJECT } from '@eurekabox/web-core';
+
+i18n.use(ChainedBackend)
+    .use(new LanguageDetector(null, { lookupLocalStorage: `@${PROJECT}_${ENV}.${LANGUAGE_KEY}` }))
     .use(initReactI18next)
     .init({
         fallbackLng: 'en',
@@ -14,6 +18,9 @@ i18n.use(Backend)
             escapeValue: false,
         },
         debug: process.env.NODE_ENV === 'development',
+        backend: {
+            backends: [LocalStorageBackend, Backend],
+        },
     });
 
 export default i18n;
--- libs/web-core/src/core/index.ts
+++ libs/web-core/src/core/index.ts
@@ -5,6 +5,8 @@ export const PROJECT = import.meta.env.VITE_PROJECT.toLowerCase();
 export const REGION = import.meta.env.VITE_REGION?.toLowerCase() || 'ap-northeast-2';
 export const OAUTH_ENDPOINT = import.meta.env.VITE_OAUTH_ENDPOINT.toLowerCase();
 
+export const LANGUAGE_KEY = 'i18nextLng';
+
 export const webCore = WebCoreFactory.create({
     cloud: 'aws',
     project: `${PROJECT}_${ENV}`,
